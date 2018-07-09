74762f0edf31d0990c488d0cbfa5e7386e60fa0c
homer quan <homer@Homers-MacBook-Pro.local>
2018-07-09 15:19:56 -0400

    a quick commit

--- server/app.js
+++ server/app.js
@@ -3,35 +3,49 @@
  * convospot widget
  */
 
-'use strict';
+"use strict";
 
-const $ = require('./libs/dollar').$;
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
+const $ = require("./libs/dollar").$;
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const cors = require("cors");
+const fs = require("fs");
 
-require('./libs/all_loader').loadDollar();
+require("./libs/all_loader").loadDollar();
 
 // Setup server
 var app = express();
 
-var server = http.createServer(app);
-
 app.use(cors());
 app.use(express.static("app"));
-require('./config/convospot.config')(app,$('config'));
+require("./config/convospot.config")(app, $("config"));
+require("./config/spinel.config")(app, $("config"));
 
-//overwrite config.html in dev env
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'stage') {
-	console.log('using dev config for elements');
-	require('./config/spinel.config')(app,$('config'));
+// Overwrite config.html in dev env
+if (
+	process.env.NODE_ENV === "development" ||
+	process.env.NODE_ENV === "stage"
+) {
+	var server = http.createServer(app);
+} else if (process.env.NODE_ENV === "production") {
+	var options = {
+		key: fs.readFileSync($('config').key),
+		cert: fs.readFileSync($('config').cert)
+	};
+	var server = https.createServer(options, app);
 }
 
 // Start server
 function startServer() {
-	server.listen($('config').port, $('config').ip, function() {
-		$('welcome')();
-		console.log('Express server listening on %s:%d, in %s mode', $('config').ip, $('config').port, app.get('env'));
+	server.listen($("config").port, $("config").ip, function() {
+		$("welcome")();
+		console.log(
+			"Express server listening on %s:%d, in %s mode",
+			$('config').ip,
+			$('config').port,
+			app.get('env')
+		);
 	});
 }
 
--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,25 +3,12 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip: process.env.OPENSHIFT_NODEJS_IP ||
-    process.env.IP ||
-    undefined,
-
-  // Server port
-  port: process.env.OPENSHIFT_NODEJS_PORT ||
-    process.env.PORT ||
-    80,
-
-  widgetUrl: 'http://widget.convospot.io',
-
-  socketUrl: 'http://app.convospot.io',
-
-  socketNamespace: '/convospot-widget-v2',
-
-  apiUrl: 'http://app.convospot.io/api/v2/',
-
-  logLvl: 'error',
-
-  coresWhitelist: ['http://app.convospot.io', 'http://widget.convospot.io']
+  port: 8805,
+  ip: '0.0.0.0',
+  debugMode: false,
+  widgetUrl: 'https://widget.reflen.com',
+  socketUrl: 'https://socket.reflen.com',
+  apiUrl: 'https://api.reflen.com/v2',
+  cert:'/etc/letsencrypt/live/widget.reflen.com/fullchain.pem';
+  key:'/etc/letsencrypt/live/widget.reflen.com/privkey.pem';
 };
